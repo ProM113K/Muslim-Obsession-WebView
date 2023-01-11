67b995085ec7d9090f1d0057be1c8061b2b81f83
Dimas Putra Widiyanto <putrad322@gmail.com>
2023-01-11 13:51:45 +0700

    WebView fix

--- app/src/main/java/com/example/muslimobsession/MainActivity.kt
+++ app/src/main/java/com/example/muslimobsession/MainActivity.kt
@@ -1,18 +1,51 @@
 package com.example.muslimobsession
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
 
         WindowInsetsCompat.Type.navigationBars()
 
-        val webView:WebView = findViewById(R.id.webView)
-        webView.loadUrl("https://www.muslimobsession.com/")
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        webView.loadUrl(URL)
+
+        // this will enable the javascript settings, it can also allow xss vulnerabilities
+        webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (webView.canGoBack())
+            webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+    }
+
+
+    companion object {
+        private const val URL = "https://www.muslimobsession.com/"
     }
 }
