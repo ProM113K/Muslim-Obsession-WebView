c49cb5767bacaca6c97a90efe75fe1eb677e8af2
Dimas Putra Widiyanto <putrad322@gmail.com>
2023-01-11 12:25:35 +0700

    Add splash screen

--- app/src/main/java/com/example/muslimobsession/MainActivity.kt
+++ app/src/main/java/com/example/muslimobsession/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.muslimobsession
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WindowInsetsCompat.Type.navigationBars()
 
         val webView:WebView = findViewById(R.id.webView)
         webView.loadUrl("https://www.muslimobsession.com/")
