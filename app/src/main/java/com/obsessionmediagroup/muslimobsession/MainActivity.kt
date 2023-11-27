15d351ef1c1de0ad6f8e0d59e93eaf91269beeba
Dimas Putra Widiyanto <putrad322@gmail.com>
2023-11-27 16:34:20 +0700

    Update targetSdk ke 34, dan versionCode ke 203

--- app/src/main/java/com/obsessionmediagroup/muslimobsession/MainActivity.kt
+++ app/src/main/java/com/obsessionmediagroup/muslimobsession/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.obsessionmediagroup.muslimobsession
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @Suppress("DEPRECATED")
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         // WebViewClient allows you to handle
         // onPageFinished and override Url loading.
-        webView.webViewClient = WebViewClient()
+        webView.webViewClient = MyWebViewClient()
 
         // this will load the url of the website
         webView.loadUrl(URL)
@@ -46,6 +53,49 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
     }
 
+    inner class MyWebViewClient : WebViewClient() {
+        @Deprecated("Deprecated in Java")
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            url: String
+        ): Boolean {
+            if (URLUtil.isNetworkUrl(url)) {
+                return false
+            } else if (url.startsWith("whatsapp:")) {
+                try {
+                    val sendIntent = Intent().apply {
+                        this.action = Intent.ACTION_SEND
+                        this.putExtra(Intent.EXTRA_TEXT, url)
+                        this.type = "text/plain"
+                    }
+                    startActivity(sendIntent)
+                    Toast.makeText(this@MainActivity, "Mengirim berita", Toast.LENGTH_SHORT).show()
+                } catch (e: ActivityNotFoundException) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Whatsapp belum terpasang",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return true
+            } else if (url.startsWith("mailto:")) {
+                try {
+                    val sendMailIntent = Intent().apply {
+                        this.action = Intent.ACTION_SEND
+                        this.putExtra(Intent.EXTRA_EMAIL, url)
+                        this.type = "text/plain"
+                    }
+                    startActivity(sendMailIntent)
+                    Toast.makeText(this@MainActivity, "Mengirim berita", Toast.LENGTH_SHORT).show()
+                } catch (e: ActivityNotFoundException) {
+                    Toast.makeText(this@MainActivity, "Gagal mengirim email", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                return true
+            }
+            return false
+        }
+    }
 
     companion object {
         private const val URL = "https://www.muslimobsession.com/"
